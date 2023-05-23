d6dc226d0bf3c8de261188d947f59f4b8518b6cf
kartikmodi25 <modikartik09@gmail.com>
2023-05-23 13:11:54 +0530

    Revert "Unnecessary comments removed"
    This reverts commit 9bcf3d0b030055ae0156511dd7d7594755bf275d.

--- main.go
+++ main.go
@@ -12,9 +12,23 @@ var db *sql.DB
 
 func main() {
 	db = utils.GetConnection()
+	// fmt.Println(db)
 	services.SetDB(db)
 	defer db.Close()
+	// fmt.Println(db)
+	// var err error
+	// db, err = sql.Open("postgres", "postgres://postgres:252900@localhost/postgres2?sslmode=disable")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+
+	// Setup router;
 	r := router.SetupRouter()
+	// fmt.Println(r)
+	// log.Println("Listening on Port 8080")
+	// log.Fatal(http.ListenAndServe(":8080", r))
+	// // // Run the server
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
